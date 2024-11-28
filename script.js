e23fc57b2ede2ca0a17b1d5934313bd15944594a
federicodececco <federicodececco1@gmail.com>
2024-11-28 15:08:51 +0100

    add: final alert

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let flag = 1;
 
-const km = prompt("inserire il numero di km da percorrere");
-/*     if(isNumber(km)) */
-console.log(isNumber(km))
+let finalPrice;
+const kilometraggio = prompt("inserire numero di km da perocrrere");
+const age = prompt("inserire età viaggiatore");
+if (age > 65) {
+    finalPrice = 0.21 * kilometraggio * (0.6)
+}
+else if (age < 18) {
+    finalPrice = 0.21 * kilometraggio * (0.8);
+
+}
+else {
+    finalPrice = 0.21 * kilometraggio;
+}
+alert(`Il prezzo del viaggio applicando gli sconti appropriati è di: ${finalPrice.toFixed(2)}`);
