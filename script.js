9ef63538b865df727a34ed4d142c379d89a8eb47
federicodececco <federicodececco1@gmail.com>
2024-11-28 16:10:41 +0100

    add: age check

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 /* variable necessary for calculations */
 let finalPrice;
-/* declaration of kilometraggio now to pass it out of while cycle */
+/* declaration of kilometraggio and age now to pass it out of while cycle */
 let kilometraggio;
-/* prompt the user for infos */
+let age;
 
+/* variable needed for whiel */
 let flag = 0;
+/* check if km is valid */
 while (flag === 0) {
     kilometraggio = prompt("inserire numero di km da perocrrere");
     if (!isNaN(kilometraggio)) {
         flag = 1;
     }
 }
-const age = prompt("inserire età viaggiatore");
+flag = 0;
+/* check if age is valid */
+while (flag === 0) {
+    age = prompt("inserire età del viaggiatore");
+    if (!isNaN(age) && age > 0) {
+        flag = 1;
+    }
+}
 /* ifs are necessary to apply the neccessary discounts */
 if (age > 65) {
     finalPrice = 0.21 * kilometraggio * (0.6)
