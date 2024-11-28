6202ef77dedc29fcfd71e75bc347c08528802306
federicodececco <federicodececco1@gmail.com>
2024-11-28 15:10:34 +0100

    add:comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-
+/* variable necessary for calculations */
 let finalPrice;
+/* prompt the user for infos */
 const kilometraggio = prompt("inserire numero di km da perocrrere");
 const age = prompt("inserire età viaggiatore");
+/* ifs are necessary to apply the neccessary discounts */
 if (age > 65) {
     finalPrice = 0.21 * kilometraggio * (0.6)
 }
@@ -12,4 +14,5 @@ else if (age < 18) {
 else {
     finalPrice = 0.21 * kilometraggio;
 }
+/* final alert that displays the price rounded up to the 2nd decimal */
 alert(`Il prezzo del viaggio applicando gli sconti appropriati è di: ${finalPrice.toFixed(2)}`);
